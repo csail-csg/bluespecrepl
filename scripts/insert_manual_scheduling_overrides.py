921d214abf02f81fdf17fae9a5b8e861fcae8994
thomas emile bourgeat <bthom@wolff.csail.mit.edu>
2017-12-08 11:31:58 -0500

    Create severaL

--- scripts/insert_manual_scheduling_overrides.py
+++ scripts/insert_manual_scheduling_overrides.py
@@ -110,8 +110,6 @@ def generate_simple_c_wrapper(module_name, scheduling_signals_in_order):
     c_wrapper += '#include "verilated.h"\n'
     c_wrapper += '#include "V%s.h"\n' % module_name
     c_wrapper += '\n'
-    c_wrapper += 'V%s* top = nullptr;\n' % module_name
-    c_wrapper += '\n'
     c_wrapper += 'const char* rules[] = {"' + ('","'.join(scheduling_signals_in_order)) + '"};\n'
     c_wrapper += 'const int num_rules = ' + str(len(scheduling_signals_in_order)) + ';\n'
     c_wrapper += '\n'
@@ -119,7 +117,8 @@ def generate_simple_c_wrapper(module_name, scheduling_signals_in_order):
     c_wrapper += 'const char** get_rules() {\n    return rules;\n}\n'
     c_wrapper += '\n'
     c_wrapper += 'extern "C"\n'
-    c_wrapper += 'int construct() {\n'
+    c_wrapper += 'V%s* construct() {\n' % module_name
+    c_wrapper += '    V%s* top = new V%s();\n' % module_name
     c_wrapper += '    Verilated::commandArgs(0, (const char**) nullptr);\n'
     c_wrapper += '    if (top != nullptr) {\n'
     c_wrapper += '        delete top;\n'
@@ -137,6 +136,7 @@ def generate_simple_c_wrapper(module_name, scheduling_signals_in_order):
     c_wrapper += '    top->eval();\n'
     c_wrapper += '    top->RST_N = 1; top->CLK = 0;\n'
     c_wrapper += '    top->eval();\n'
+    c_wrapper += '    return top;\n'
     c_wrapper += '}\n'
     c_wrapper += 'extern "C"\n'
     c_wrapper += 'int set_CLK(int x) {\n'
